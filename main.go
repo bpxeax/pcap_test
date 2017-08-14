a61594ab44c1905d45bcdb1cb5dff8a1d2108f8e
bpxeax <bpxeax@gmail.com>
2017-08-14 08:55:02 +0800

    show info

--- main.go
+++ main.go
@@ -5,27 +5,52 @@ import (
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcap"
+	"io"
 )
 
-func handlePacket(packet gopacket.Packet) {
-	if tcpLayer := packet.Layer(layers.LayerTypeTCP); tcpLayer != nil {
-		tcp, _ := tcpLayer.(*layers.TCP)
-		fmt.Printf("src port: %d, dst port: %d\n", tcp.SrcPort, tcp.DstPort)
+func handlePacket(packetData []byte) {
+	packet := gopacket.NewPacket(packetData, layers.LayerTypeEthernet, gopacket.Default)
+
+	if udpLayer := packet.Layer(layers.LayerTypeUDP); udpLayer != nil {
+		udp, _ := udpLayer.(*layers.UDP)
+		fmt.Printf("src port: %d, dst port: %d\n", udp.SrcPort, udp.DstPort)
 	}
+
+	netFlow := packet.NetworkLayer().NetworkFlow()
+	fmt.Printf("src ip: %s; dst ip: %s\n", netFlow.Src().String(), netFlow.Dst().String())
+
+	if appLayer := packet.ApplicationLayer(); appLayer != nil {
+		fmt.Printf("data: %s\n", string(appLayer.LayerContents()))
+	}
+
 }
 
 func main() {
 	fmt.Println("starting...")
 	if handle, err := pcap.OpenLive("enp6s0", 1600, true, 0); err != nil {
 		panic(err)
-	} else if err := handle.SetBPFFilter("tcp and (port 80 or port 8080 or port 443)"); err != nil {
+	} else if err := handle.SetBPFFilter("udp and (port 10002)"); err != nil {
 		panic(err)
 	} else {
-		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-		for packet := range packetSource.Packets() {
-			handlePacket(packet)
-			fmt.Println("heheda")
-			handle.WritePacketData(packet.Data())
+		if handleWrite, err := pcap.OpenLive("enp6s0", 65536, true, pcap.BlockForever); err != nil {
+			panic(err)
+		} else {
+			for {
+				data, _, err := handle.ReadPacketData()
+				switch {
+				case err == io.EOF:
+					fmt.Println("finished!")
+					return
+				case err != nil:
+					fmt.Printf("fail to read packet, error: %s", err)
+				default:
+					handlePacket(data)
+					err := handleWrite.WritePacketData(data)
+					if err != nil {
+						fmt.Println("send error: ", err)
+					}
+				}
+			}
 		}
 	}
 }
